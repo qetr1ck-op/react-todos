5695c95e4f6e8bc0afed5981215b51efa5f9fcd8
qetr1ck-op <orestprustayko@gmail.com>
2019-02-15 22:30:02 +0200

    feature::tweek

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -51,7 +51,7 @@ const useTypeScript = fs.existsSync(paths.appTsConfig)
 
 // style files regexes
 const cssRegex = /\.css$/
-const cssModuleRegex = /(\.module\)?.css$/
+const cssModuleRegex = /(\.module\.)?css$/
 const sassRegex = /\.(scss|sass)$/
 const sassModuleRegex = /\.module\.(scss|sass)$/
 
