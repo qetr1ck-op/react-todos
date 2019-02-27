37deb0e2f4c5c0c9e591cc40a669ea93802010be
qetr1ck-op <orestprustayko@gmail.com>
2019-02-27 22:59:15 +0200

    test precommit5

--- src/pages/root/root.page.tsx
+++ src/pages/root/root.page.tsx
@@ -1,7 +1,7 @@
 import createBrowserHistory from 'history/createBrowserHistory'
 import DevTool from 'mobx-react-devtools'
-import { RouterStore, syncHistoryWithStore } from 'mobx-react-router'
 import { Route, Router } from 'react-router'
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router'
 import * as React from 'react'
 
 import { Todos, TodosStoreProvider } from '../todos'
@@ -14,6 +14,8 @@ const history = syncHistoryWithStore(browserHistory, routerStore)
 export function RootPage() {
   return (
     <>
+
+
       <Router history={history}>
         <Route
           exact
